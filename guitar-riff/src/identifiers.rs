9aa0ada9bbe88f6ceeaf0aa6d3b74e5d8305be24
toneo- <toneo-@users.noreply.github.com>
2023-07-13 01:11:27 +0100

    Add INFO identifier and document these identifiers

--- guitar-riff/src/identifiers.rs
+++ guitar-riff/src/identifiers.rs
@@ -1,4 +1,8 @@
 use crate::chunk::ChunkIdentifier;
 
+/// Built-in [`ChunkIdentifier`] for the "INFO" list chunk, which contains metadata.
+pub const INFO: ChunkIdentifier = ChunkIdentifier(*b"INFO");
+/// Built-in [`ChunkIdentifier`] for the "LIST" chunk, which can contain sub-chunks.
 pub const LIST: ChunkIdentifier = ChunkIdentifier(*b"LIST");
+/// Built-in [`ChunkIdentifier`] for the "RIFF" chunk, which represents a RIFF file and can contain sub-chunks.
 pub const RIFF: ChunkIdentifier = ChunkIdentifier(*b"RIFF");
